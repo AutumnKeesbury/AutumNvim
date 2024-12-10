3e7e491578f9db11dca15f13d9d8ffaef71ac79a
Autumn Keesbury <116507614+AutumnKeesbury@users.noreply.github.com>
2024-12-10 00:40:18 -0600

    Update alpha.lua

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -25,29 +25,11 @@ return {
       { type = "text", val = "Autumn's NeoVIM (◕‿◕✿)", opts = { hl = "NeovimDashboardUsername", shrink_margin = false, position = "center" }, },
     }
     -- stylua: ignore
-    dashboard.section.buttons.val = {
-      dashboard.button("f", " " .. " Find file",       "<cmd> Telescope find_files <cr>"),
-      dashboard.button("n", " " .. " New file",        "<cmd> ene <BAR> startinsert <cr>"),
-      dashboard.button("r", " " .. " Recent files",    "<cmd> Telescope oldfiles <cr>"),
-      dashboard.button("g", " " .. " Find text",       "<cmd> Telescope live_grep <cr>"),
-      dashboard.button("Z", " " .. " Open Directories", "<cmd> lua require('telescope').extensions.zoxide.list() <cr>"),
-      dashboard.button("c", " " .. " Config",          "<cmd> lua require('lazyvim.util').telescope.config_files()() <cr>"),
-      dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
-    --dashboard.button("x", " " .. " Lazy Extras",     "<cmd> LazyExtras <cr>"),
-      dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
-      dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
-    }
-    vim.api.nvim_set_hl(0, "AlphaHeader", { fg = "#311B92" }) -- Dark Indigo
-    vim.api.nvim_set_hl(0, "AlphaButtons", { fg = "#D1C4E9" }) -- Light Purple
-    vim.api.nvim_set_hl(0, "AlphaShortcut", { fg = "#8BC34A" }) -- Greenish
+    vim.api.nvim_set_hl(0, "AlphaHeader", { fg = "#ee964b" }) -- Orange-Tan
+    vim.api.nvim_set_hl(0, "AlphaShortcut", { fg = "ce4257" }) -- Light Magenta
     vim.api.nvim_set_hl(0, "AlphaFooter", { fg = "#edd691" })
 
-    for _, button in ipairs(dashboard.section.buttons.val) do
-      button.opts.hl = "AlphaButtons"
-      button.opts.hl_shortcut = "AlphaShortcut"
-    end
     dashboard.section.header.opts.hl = "AlphaHeader"
-    dashboard.section.buttons.opts.hl = "AlphaButtons"
     dashboard.section.footer.opts.hl = "AlphaFooter"
     dashboard.opts.layout[1].val = 3
     return dashboard
